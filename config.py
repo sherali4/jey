288ff98b5f4b2a6b98bdac6f1ef725be582af0c6
User <sher5624067@gmail.com>
2023-11-07 19:13:59 +0300

    after clear code

---
+++ config.py
@@ -0,0 +1,9 @@
+TOKEN = '6682195858:AAFGE7CYgx4RR8n04FNBmkCZY_Tn6UM5u58'
+gruppa = '-1001989106073'
+gruppa_sale = '-1001715961258'
+sherali ='496958227'
+# bot_id = '6368002025'
+xa_yuq = '-1001605076473'
+group = '@jeypaygroup'
+# env1\bin\python -m pip freeze > requirements.txt
+# env2\bin\python -m pip install -r requirements.txt
